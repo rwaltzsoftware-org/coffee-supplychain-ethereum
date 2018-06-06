fe9d4bd34f8c8d57dd08e23f6c993c5605f1c0d4
Nitish Kasar <nitish@imperialsoftech.com>
2018-06-06 19:04:30 +0530

    - Added Details for Cultivation Farm Inspection and Harvest

--- test/SupplyChainStorage.js
+++ test/SupplyChainStorage.js
@@ -15,7 +15,7 @@ contract('SupplyChainStorage',function(accounts)
 		const authorizeStatus = await instanceSupplyChainStorage.authorizeCaller(authorizedCaller,{from:spenderAddress});
 
 		console_log("Checking AuthorizedCaller Event: ");
-		
+
 		assert.web3Event(authorizeStatus,{
 			event: 'AuthorizedCaller',
 			args: {
@@ -93,12 +93,27 @@ contract('SupplyChainStorage',function(accounts)
 		
 	});
 
-	it("Set/Get Cultivation Basic Details ",async() => {
+	it("Set/Get Cultivation",async() => {
 
 		const instanceSupplyChainStorage = await SupplyChainStorage.deployed();
 		const authorizedCaller = accounts[1];
 
-		const watcher = instanceSupplyChainStorage.PerformCultivation();
+		const basicDetailsWatcher = instanceSupplyChainStorage.PerformCultivation();
+		const inspectionWatcher = instanceSupplyChainStorage.DoneInspection();
+		const harvestWatcher = instanceSupplyChainStorage.DoneHarvesting();
+		const exportWatcher = instanceSupplyChainStorage.DoneExporting();
+		const importWatcher = instanceSupplyChainStorage.DoneImporting();
+		const processorWatcher = instanceSupplyChainStorage.DoneProcessing();
+
+		
+
+		/* Authorized Caller */
+		console_log("Authorizing User: " + authorizedCaller);
+		const authorizeStatus = await instanceSupplyChainStorage.authorizeCaller(authorizedCaller,{from:spenderAddress});
+
+		/********************* Basic Details Section ***********/
+
+		/* Set Basic Details */
 
 		var _registrationNo = "123456789";
 		var _farmerName = "Ramu Kaka";
@@ -106,26 +121,24 @@ contract('SupplyChainStorage',function(accounts)
 		var _exporterName = "Rudra Logistics";
 		var _importerName = "Boulders Logistics";
 
-		/* Authorized Caller */
-		console_log("Authorizing User: " + authorizedCaller);
-		const authorizeStatus = await instanceSupplyChainStorage.authorizeCaller(authorizedCaller,{from:spenderAddress});
 
-		const status = await instanceSupplyChainStorage.setBasicDetails(
+		const basicDetailsStatus = await instanceSupplyChainStorage.setBasicDetails(
 																	_registrationNo,
 																	_farmerName,
 																    _farmAddress,
 																    _exporterName,
 																    _importerName,{from:authorizedCaller});
 
-		const events = await watcher.get();
+		const basicDetailsEvent = await basicDetailsWatcher.get();
 
+		/* Check Event wise */
 		console_log("Checking PerformCultivation Event: ");
 
-		assert.web3Event(status,{
+		assert.web3Event(basicDetailsStatus,{
 			event:'PerformCultivation',
 			args:{
 				user:authorizedCaller,
-				batchNo:events[0].args.batchNo
+				batchNo:basicDetailsEvent[0].args.batchNo
 			}
 		},"PerformCultivation");
 
@@ -133,15 +146,96 @@ contract('SupplyChainStorage',function(accounts)
 		/* Check Function Call wise  */
 		console_log("Checking Get Basic Details : ");
 
-		const user = await instanceSupplyChainStorage.getBasicDetails(events[0].args.batchNo,{from:authorizedCaller});			
+		const basicDetailsData = await instanceSupplyChainStorage.getBasicDetails(basicDetailsEvent[0].args.batchNo,{from:authorizedCaller});			
 
-		assert.equal(user[0],_registrationNo,"Registration No Check:");
-		assert.equal(user[1],_farmerName,"Farmer Name Check:");
-		assert.equal(user[2],_farmAddress,"Farmer Address Check:");
-		assert.equal(user[3],_exporterName,"Exporter Check:");
-		assert.equal(user[4],_importerName,"Importer Check:");
+		assert.equal(basicDetailsData[0],_registrationNo,"Registration No Check:");
+		assert.equal(basicDetailsData[1],_farmerName,"Farmer Name Check:");
+		assert.equal(basicDetailsData[2],_farmAddress,"Farmer Address Check:");
+		assert.equal(basicDetailsData[3],_exporterName,"Exporter Check:");
+		assert.equal(basicDetailsData[4],_importerName,"Importer Check:");
+
+		/********************* Farm Inspection Section ***********/
+
+		/* Set Basic Details */
+
+		var batchNo = basicDetailsEvent[0].args.batchNo;
+		var _coffeeFamily = "Rubiaceae";
+		var _typeOfSeed = "Coffee robusta";
+		var _fertilizerUsed = "Organic";
 
 
+		const inspectionStatus = await instanceSupplyChainStorage.setFarmInspectorData(
+																	batchNo,
+																	_coffeeFamily,
+																    _typeOfSeed,
+																    _fertilizerUsed,{from:authorizedCaller});
+
+		const inspectionEvent = await inspectionWatcher.get();
+
+		/* Check Event wise */
+		console_log("Checking DoneInspection Event: ");
+
+		assert.web3Event(inspectionStatus,{
+			event:'DoneInspection',
+			args:{
+				user:authorizedCaller,
+				batchNo:inspectionEvent[0].args.batchNo
+			}
+		},"DoneInspection");
+
+
+		/* Check Function Call wise  */
+		console_log("Checking Get Farm Inspection : ");
+
+		const inspectionData = await instanceSupplyChainStorage.getFarmInspectorData(inspectionEvent[0].args.batchNo,{from:authorizedCaller});			
+
+		assert.equal(inspectionData[0],_coffeeFamily,"coffeeFamily Check:");
+		assert.equal(inspectionData[1],_typeOfSeed,"_typeOfSeed Check:");
+		assert.equal(inspectionData[2],_fertilizerUsed,"_fertilizerUsed Check:");
+
+
+		/********************* Harvestor Section ***********/
+
+
+		var _coffeeFamily = "Rubiaceae";
+		var _tempatureUsed = "60 Degree";
+		var _humidity = "95";
+
+
+		const harvestStatus = await instanceSupplyChainStorage.setHarvesterData(
+																	batchNo,
+																	_coffeeFamily,
+																    _tempatureUsed,
+																    _humidity,{from:authorizedCaller});
+
+		const harvestEvent = await harvestWatcher.get();
+
+		/* Check Event wise */
+		console_log("Checking DoneHarvesting Event: ");
+
+		assert.web3Event(harvestStatus,{
+			event:'DoneHarvesting',
+			args:{
+				user:authorizedCaller,
+				batchNo:harvestEvent[0].args.batchNo
+			}
+		},"DoneHarvesting");
+
+
+		/* Check Function Call wise  */
+		console_log("Checking Get Harvestor : ");
+
+		const harvestData = await instanceSupplyChainStorage.getHarvesterData(harvestEvent[0].args.batchNo,{from:authorizedCaller});			
+
+		assert.equal(harvestData[0],_coffeeFamily,"_coffeeFamily Check:");
+		assert.equal(harvestData[1],_tempatureUsed,"_tempatureUsed Check:");
+		assert.equal(harvestData[2],_humidity,"_humidity Check:");
+
+
+		
+
+
+		
 	});
 })
 
