a6e7a5e26ac4ee9c6ac2e2c1571dc879096483ad
Nitish Kasar <nitish@imperialsoftech.com>
2018-06-08 11:14:33 +0530

    - Removed process.exit from migration

--- test/CoffeeSupplyChain.js
+++
@@ -1,8 +0,0 @@
-/*Require for Events*/
-require('truffle-test-utils').init();
-
-var CoffeeSupplyChain = artifacts.require('../contracts/CoffeeSupplyChain.sol');
-
-contract('CoffeeSupplyChain',function(accounts){
-	console.log(accounts);
-});
