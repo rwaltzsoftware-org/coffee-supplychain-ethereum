f5bc10e619921efff78e5240db185120aa706c66
vikas <vikas@imperialsoftech.com>
2018-09-08 18:05:54 +0530

    Truffle Deployment Script Updated

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,12 +6,13 @@ var SupplyChainUser = artifacts.require("./SupplyChainUser");
 module.exports = function(deployer){
 	deployer.deploy(SupplyChainStorage)
 	.then(()=>{
+		return deployer.deploy(CoffeeSupplyChain,SupplyChainStorage.address);
+	})
+	.then(()=>{
+		return deployer.deploy(SupplyChainUser,SupplyChainStorage.address);
+	})
+	.then(()=>{
    		return SupplyChainStorage.deployed();
-    }).then(async (SupplyChainStorageInstance)=>{
-		await Promise.all([deployer.deploy(CoffeeSupplyChain,SupplyChainStorage.address),
-			   			deployer.deploy(SupplyChainUser,SupplyChainStorage.address)]);
-	   
-	    return SupplyChainStorageInstance;
     }).then(async function(instance){
 		await instance.authorizeCaller(CoffeeSupplyChain.address); 
 		await instance.authorizeCaller(SupplyChainUser.address);
